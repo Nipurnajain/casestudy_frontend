f701d72728cdf5b41ee805ed637905692ba7cc9d
unknown <nipurnabandi19@gmail.com>
2024-02-19 17:47:46 +0530

    added restaurant form

--- src/app/adminDashboard/add-restaurant/add-restaurant.component.ts
+++ src/app/adminDashboard/add-restaurant/add-restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-restaurant',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AddRestaurantComponent {
 
+  registerRestaurantForm!:FormGroup;
+  submitted = false;
+  constructor(private formBuilder:FormBuilder) { 
+
+  }
+
+  ngOnInit(){
+    this.registerRestaurantForm= this.formBuilder.group({
+      restaurantName : ['',[Validators.required,Validators.pattern('^[a-zA-Z\\s]+$')]],
+      location : ['',[Validators.required,Validators.pattern('^[a-zA-Z\\s]+$')]],
+      contact : ['',[Validators.required,Validators.pattern('[0-9]{10}')]],
+      rating :['',[Validators.required,Validators.pattern('[0-5]')]]
+    });
+  }
+
+  get f(){
+    return this.registerRestaurantForm.controls;
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.registerRestaurantForm.invalid){
+      return
+    }
+    alert('SUCCESS!! :-)')
+  }
 }
