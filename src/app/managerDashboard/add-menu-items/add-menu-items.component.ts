0d39689d2bd022227ced0f5fb4e514ecd849c525
unknown <nipurnabandi19@gmail.com>
2024-02-23 22:03:00 +0530

    added add-menuitem form

--- src/app/managerDashboard/add-menu-items/add-menu-items.component.ts
+++ src/app/managerDashboard/add-menu-items/add-menu-items.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ManagerService } from '../manager.service';
+import { Router } from '@angular/router';
+import { Restaurant } from 'src/app/adminDashboard/Restaurant';
+import { MenuItem } from '../MenuItem';
 
 @Component({
   selector: 'app-add-menu-items',
@@ -6,5 +11,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-menu-items.component.css']
 })
 export class AddMenuItemsComponent {
+  registerMenuItemForm!: FormGroup;
+  selectedFile!: File;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder, private managerService: ManagerService, private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.registerMenuItemForm= this.formBuilder.group({
+      itemName: ['', [Validators.required, Validators.pattern('^[a-zA-Z\\s]+$')]],
+      description: ['', [Validators.required, Validators.pattern('^[a-zA-Z\\s]+$')]],
+      category: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      availabilityTime: ['', [Validators.required]],
+      specialDietaryInfo: ['', [Validators.required]],
+      tasteInfo:['', [Validators.required]],
+      nutritionalInfo: ['', [Validators.required]],
+      cookingTime: ['', [Validators.required]],
+      restaurantId: ['', [Validators.required]]
+    });
+  }
+
+  get f() {
+    return this.registerMenuItemForm.controls;
+  }
+
+  onFileSelected(event: { target: { files: File[]; }; }) {
+    this.selectedFile = event.target.files[0] as File;
+  }
+
+
+  insertMenuItem(data: MenuItem) {
+    this.managerService.addMenuItem(data)
+      .subscribe(
+        (menu) => { console.log('Added menu item is: ' + menu);
+        this.router.navigate(['/manager-dashboard/display-menuitems']); }
+      );
+
+
+  }
+
 
 }
--- src/app/managerDashboard/manager.service.ts
+++ src/app/managerDashboard/manager.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Restaurant } from '../adminDashboard/Restaurant';
+import { MenuItem } from './MenuItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('jwtToken');
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  }
+  
+
+
+
+
+  addMenuItem(body: MenuItem): Observable<MenuItem> {
+
+    console.log(body);
+
+    return this.http.post<MenuItem>("http://localhost:8080/api/v1/restaurant/addMenu/+ `/${restaurantId}`", body,{ headers: this.getHeaders(), responseType: 'text' as 'json' });
+
+  }
 }
