4287ac6cd73aef0e1acd01540451c9b80797ba7c
unknown <nipurnabandi19@gmail.com>
2024-02-24 15:55:50 +0530

    added displayorders, menuitems

---
+++ src/app/adminDashboard/Customer.ts
@@ -0,0 +1,9 @@
+export interface Customer {
+    custId:number;
+    custName: string,
+    gender: string;
+    email: string;
+    phone: string;
+    username: string;
+
+}
---
+++ src/app/managerDashboard/display-discount/display-discount.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-discount',
+  templateUrl: './display-discount.component.html',
+  styleUrls: ['./display-discount.component.css']
+})
+export class DisplayDiscountComponent {
+
+}
